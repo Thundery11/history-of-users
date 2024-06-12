47efb61f7113d447791dfa2f6848ce50cec3d9fb
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2024-06-12 14:35:57 +0300

    dsdsds

---
+++ apps/users/src/users.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+export const options: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  autoLoadEntities: true,
+  synchronize: true,
+  ssl: true,
+};
+
+@Module({
+  imports: [TypeOrmModule.forRoot(options)],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule {}
